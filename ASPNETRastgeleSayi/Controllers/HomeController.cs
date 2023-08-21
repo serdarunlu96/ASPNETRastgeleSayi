4f59b22d52ce1f6477fded4e43713a82694a5bda
serdarunlu96 <71671958+serdarunlu96@users.noreply.github.com>
2023-08-21 23:26:49 +0300

    Add project files.

---
+++ ASPNETRastgeleSayi/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNETRastgeleSayi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPNETRastgeleSayi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Gonder(string Ad)
+        {
+            ViewData["Ad"] = Ad;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
